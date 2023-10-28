21617798ea6a6f79f1c136e485624f7b8aa8a3bd
dornss <gabrielldornelas@hotmail.com>
2023-10-28 18:47:03 -0300

    feat: first MVP

--- src/main/java/br/com/ibm/bancoservlet/controllers/DepositarServlet.java
+++ src/main/java/br/com/ibm/bancoservlet/controllers/DepositarServlet.java
@@ -1,46 +1,37 @@
 package br.com.ibm.bancoservlet.controllers;
 
 import br.com.ibm.bancoservlet.GlobalExceptionHandler.ContaInvalidaException;
-import br.com.ibm.bancoservlet.models.*;
+import br.com.ibm.bancoservlet.models.ContaCorrente;
+import br.com.ibm.bancoservlet.services.ContaCorrenteService;
+import br.com.ibm.bancoservlet.services.ContaCorrenteServiceImpl;
 import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/depositar")
 public class DepositarServlet extends HttpServlet {
+
+    private final List<ContaCorrente> contasCorrentes = new ArrayList<>();
+    private final ContaCorrenteService contaCorrenteService = new ContaCorrenteServiceImpl(contasCorrentes);
+
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String numeroConta = req.getParameter("numeroConta");
-        double valorDeposito = Double.parseDouble(req.getParameter("valorDeposito"));
-
-        ServletContext servletContext = getServletContext();
-        List<ContaCorrente> contasCorrentes = (List<ContaCorrente>) servletContext.getAttribute("contasCorrentes");
-
-        ContaCorrente contaDeposito = null;
-        for (ContaCorrente conta : contasCorrentes) {
-            if (conta.getNumeroDaConta().equals(numeroConta)) {
-                contaDeposito = conta;
-                break;
-            }
-        }
+        double valorDeposito = Double.parseDouble(req.getParameter("valorSaque"));
 
         try {
-            if (contaDeposito != null) {
-                double novoSaldo = contaDeposito.getSaldo() + valorDeposito;
-                contaDeposito.setSaldo(novoSaldo);
-                resp.sendRedirect("clientes.jsp");
-            } else {
-                throw new ContaInvalidaException("Conta inválida. Por favor, verifique o número da conta.");
-            }
+            contaCorrenteService.depositar(numeroConta, valorDeposito);
+            resp.sendRedirect("index.jsp");
         } catch (ContaInvalidaException e) {
             req.setAttribute("mensagemErro", e.getMessage());
             RequestDispatcher rq = req.getRequestDispatcher("erro.jsp");
             rq.forward(req, resp);
         }
+
     }
 }
